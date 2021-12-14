68a5b8529e48b7b3bc6022696aa11fd4b4a5683a
klauslawson <klauslawsondjecky56@gmail.com>
2021-12-14 12:41:25 -0600

    second commit

--- taskBlog/src/app/app.module.ts
+++ taskBlog/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ButtonComponent } from './components/button/button.component';
     ButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- taskBlog/src/app/components/button/button.component.ts
+++ taskBlog/src/app/components/button/button.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
 
+  model:any = {
+    left: true,
+    middle: false,
+    right: false
+  };
   constructor() { }
 
   ngOnInit(): void {
